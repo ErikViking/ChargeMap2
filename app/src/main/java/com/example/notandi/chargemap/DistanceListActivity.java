1d90a0df90b4bea84be34a8ed36cc9f5be01004e
notandi <krikur@gmail.com>
2014-11-09 16:45:37 +0100

    More list on Ubuntu Lappy

--- app/src/main/java/com/example/notandi/chargemap/DistanceListActivity.java
+++ app/src/main/java/com/example/notandi/chargemap/DistanceListActivity.java
@@ -1,6 +1,13 @@
 package com.example.notandi.chargemap;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by notandi on 08.11.14.
@@ -8,13 +15,47 @@ import android.app.Activity;
 public class DistanceListActivity extends Activity {
 
     DBConnect db = new DBConnect(this);
-    double[][] list = db.getList();
+    Navigator nav;
+
+    double[][] list;
+    LatLng latLng;
+    ArrayList<GPSCoordinate> coordinate;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
+
+
+    private void getList(){
+        list = db.getList();
+    }
+
+    private void populateArraylist(){
+        int listIndex = 0;
+        int rows = list.length;
+        for (int i = 0; i < list.length; i++) {
+
+        }
+    }
 
 
 
 }
 
 
+/*
+
+    public void getList() {
+        latlngs = new ArrayList<LatLng>();
+        double[][] list = db.getList();
+        int listIndex = 0;
+        int rows = list.length;
+        for (int i = 0; i < list.length; i++) {
+
+        }
+
+    }
+ */
---
+++ app/src/main/java/com/example/notandi/chargemap/GPSCoordinate.java
@@ -0,0 +1,21 @@
+package com.example.notandi.chargemap;
+
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by notandi on 09.11.14.
+ */
+public class GPSCoordinate {
+
+    public double lat;
+    public double lon;
+    //public double clearlineDist;
+    //public double
+
+    public GPSCoordinate(Location location, LatLng latlng) {
+        super();
+
+    }
+}
